629126273c6d13f73be474508f4dca71931e1605
MateoCoriaChris <106635887+MateoCoriaChris@users.noreply.github.com>
2023-02-19 22:55:40 -0300

    Add files via upload

---
+++ EntregaFinal(Heladeria)Coria/JS/main.js
@@ -0,0 +1,103 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const showAlert = document.getElementById("showAlert");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+productos.forEach((product) => {
+  let content = document.createElement("div");
+  content.className = "card";
+  content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="price">${product.precio} $</p>
+  `;
+
+  shopContent.append(content);
+
+  let comprar = document.createElement("button");
+  comprar.innerText = "comprar";
+  comprar.className = "comprar";
+
+  content.append(comprar);
+
+  comprar.addEventListener("click", () => {
+    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+
+    if (repeat) {
+      carrito.map((prod) => {
+        if (prod.id === product.id) {
+          prod.cantidad++;
+        }
+      });
+    } else {
+      carrito.push({
+        id: product.id,
+        img: product.img,
+        nombre: product.nombre,
+        precio: product.precio,
+        cantidad: product.cantidad,
+      });
+      console.log(carrito);
+      console.log(carrito.length);
+      carritoCounter();
+      saveLocal();
+    }
+  });
+});
+
+//set item
+const saveLocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+//get item
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+
+
+const botonAgregar = document.getElementById('agregarAlCarrito');
+botonAgregar.addEventListener('click', () => {
+  Swal.fire({
+    title: 'Está seguro que quiere agregar este producto al carrito?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, quiero agregarlo al carrito',
+    cancelButtonText: 'No, no quiero agregarlo'
+  }).then((resultado) => {
+    if (resultado.isConfirmed) {
+      Swal.fire({
+          title: 'se ha agregado el producto al carrito',
+          icon: 'success',
+          text: 'El producto a sido agregado al carrito'
+      })
+    }
+  })
+})
+
---
+++ EntregaFinal(Heladeria)Coria/JS/productos.js
@@ -0,0 +1,101 @@
+const productos = [
+    {
+      id: 1,
+      nombre: 'Helado de 1kg',
+      precio: 200,
+      descripcion: 'Gustos disponibles:chocolate,dulce de leche, frutilla, vainilla, frutos del bosque, tiramisu.',
+      img: '/imagenes/Helado de 1kg.jpeg',
+      categoria: 'Helados',
+      cantidad:1
+    },
+    {
+      id: 2,
+      nombre: 'Helado de 1/2kg',
+      precio: 1500,
+      descripcion: 'Gustos disponibles:chocolate,dulce de leche, frutilla, vainilla, frutos del bosque, tiramisu.',
+      img: '/imagenes/helado de medio kilo.jpeg',
+      categoria: 'Helados',
+      cantidad:1
+    },
+    {
+      id: 3,
+      nombre: 'Helado de 1/4kg',
+      precio: 2000,
+      descripcion: 'Gustos disponibles:chocolate,dulce de leche, frutilla, vainilla, frutos del bosque, tiramisu.',
+      img: '/imagenes/helado de un cuarto.jpeg',
+      categoria: 'Helados',
+      cantidad:1
+    },
+    {
+      id: 4,
+      nombre: 'Pastel Cheesecake',
+      precio: 500,
+      descripcion: 'Super producto 4 para el hogar',
+      img: '/imagenes/cheesecake.jpeg',
+      categoria: 'Pasteles',
+      cantidad:1
+    },
+    {
+      id: 5,
+      nombre: 'Pastel Selva Negra',
+      precio: 400,
+      descripcion: 'Super producto 5 para el hogar',
+      img: '/imagenes/selva negra.jpeg',
+      categoria: 'Pasteles',
+      cantidad:1
+    },
+    {
+      id: 6,
+      nombre: 'Chocotorta',
+      precio: 400,
+      descripcion: 'Super producto 5 para el hogar',
+      img: '/imagenes/chocotorta.jpeg',
+      categoria: 'Pasteles',
+      cantidad:1
+    },
+    {
+      id: 7,
+      nombre: 'Torta de Frutilla',
+      precio: 400,
+      descripcion: 'Super producto 5 para el hogar',
+      img: '/imagenes/torta de frutilla.jpeg',
+      categoria: 'Pasteles',
+      cantidad:1
+    },
+    {
+      id: 8,
+      nombre: 'Bombon Suizo',
+      precio: 400,
+      descripcion: 'Super producto 5 para el hogar',
+      img: '/imagenes/bombon suizo.jpeg',
+      categoria: 'Postres',
+      cantidad:1
+    },
+    {
+      id: 9,
+      nombre: 'Helados de Agua',
+      precio: 400,
+      descripcion: 'Super producto 5 para el hogar',
+      img: '/imagenes/helados de agua.jpeg',
+      categoria: 'Helados de agua',
+      cantidad:1
+    },
+    {
+      id: 10,
+      nombre: 'Conos de helado',
+      precio: 400,
+      descripcion: 'Super producto 5 para el hogar',
+      img: '/imagenes/conos de helado.jpeg',
+      categoria: 'Otros',
+      cantidad:1
+    },
+    {
+      id: 11,
+      nombre: 'Comida Congelada',
+      precio: 400,
+      descripcion: 'Super producto 5 para el hogar',
+      img: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
+      categoria: 'Comida Congelada',
+      cantidad:1
+    }
+  ];
