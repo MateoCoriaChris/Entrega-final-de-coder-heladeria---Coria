409a9a7e3d8b83f9c16a720a194d394f34ecc156
MateoCoriaChris <106635887+MateoCoriaChris@users.noreply.github.com>
2023-02-21 22:22:08 -0300

    Update main.js

--- EntregaFinal(Heladeria)Coria/JS/main.js
+++ EntregaFinal(Heladeria)Coria/JS/main.js
@@ -4,49 +4,77 @@ const modalContainer = document.getElementById("modal-container");
 const showAlert = document.getElementById("showAlert");
 const cantidadCarrito = document.getElementById("cantidadCarrito");
 
+
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-productos.forEach((product) => {
-  let content = document.createElement("div");
-  content.className = "card";
-  content.innerHTML = `
-    <img src="${product.img}">
-    <h3>${product.nombre}</h3>
-    <p class="price">${product.precio} $</p>
-  `;
 
-  shopContent.append(content);
 
-  let comprar = document.createElement("button");
-  comprar.innerText = "comprar";
-  comprar.className = "comprar";
-
-  content.append(comprar);
-
-  comprar.addEventListener("click", () => {
-    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-
-    if (repeat) {
-      carrito.map((prod) => {
-        if (prod.id === product.id) {
-          prod.cantidad++;
+const getProduct= async()=>{
+  const response= await fetch("/JS/JSON/data.json");
+  const data= await response.json();
+  data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+      <img src="${product.img}">
+      <h3>${product.nombre}</h3>
+      <p class="price">${product.precio} $</p>
+    `;
+  
+    shopContent.append(content);
+  
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+  
+    content.append(comprar);
+  
+    comprar.addEventListener("click", () => {
+      const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
         }
-      });
-    } else {
-      carrito.push({
-        id: product.id,
-        img: product.img,
-        nombre: product.nombre,
-        precio: product.precio,
-        cantidad: product.cantidad,
-      });
-      console.log(carrito);
-      console.log(carrito.length);
-      carritoCounter();
-      saveLocal();
-    }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Se ha agregado el producto al carrito'
+      })
+  
+      if (repeat) {
+        carrito.map((prod) => {
+          if (prod.id === product.id) {
+            prod.cantidad++;
+          }
+        });
+      } else {
+        carrito.push({
+          id: product.id,
+          img: product.img,
+          nombre: product.nombre,
+          precio: product.precio,
+          cantidad: product.cantidad,
+        });
+        console.log(carrito);
+        console.log(carrito.length);
+        carritoCounter();
+        saveLocal();
+      }
+      
+    });
   });
-});
+}
+
+getProduct();
+
+
 
 //set item
 const saveLocal = () => {
@@ -79,25 +107,3 @@ function w3_open() {
 function w3_close() {
     mySidebar.style.display = "none";
 }
-
-
-
-const botonAgregar = document.getElementById('agregarAlCarrito');
-botonAgregar.addEventListener('click', () => {
-  Swal.fire({
-    title: 'Está seguro que quiere agregar este producto al carrito?',
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonText: 'Si, quiero agregarlo al carrito',
-    cancelButtonText: 'No, no quiero agregarlo'
-  }).then((resultado) => {
-    if (resultado.isConfirmed) {
-      Swal.fire({
-          title: 'se ha agregado el producto al carrito',
-          icon: 'success',
-          text: 'El producto a sido agregado al carrito'
-      })
-    }
-  })
-})
-
