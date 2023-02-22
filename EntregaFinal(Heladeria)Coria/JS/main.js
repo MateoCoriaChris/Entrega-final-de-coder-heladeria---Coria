d358b39117317a10a04b4d10602322ea1da2d537
MateoCoriaChris <106635887+MateoCoriaChris@users.noreply.github.com>
2023-02-22 00:50:49 -0300

    Add files via upload

---
+++ EntregaFinal(Heladeria)Coria/JS/main.js
@@ -0,0 +1,111 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const showAlert = document.getElementById("showAlert");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
+
+const getProduct= async()=>{
+  const response= await fetch("/JS/JSON/data.json");
+  const data= await response.json();
+  data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+      <img src="${product.img}">
+      <h3>${product.nombre}</h3>
+      <p class="price">${product.precio} $</p>
+    `;
+  
+    shopContent.append(content);
+  
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+  
+    content.append(comprar);
+  
+    comprar.addEventListener("click", () => {
+      const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Se ha agregado el producto al carrito'
+      })
+  
+      if (repeat) {
+        carrito.map((prod) => {
+          if (prod.id === product.id) {
+            prod.cantidad++;
+          }
+        });
+      } else {
+        carrito.push({
+          id: product.id,
+          img: product.img,
+          nombre: product.nombre,
+          precio: product.precio,
+          cantidad: product.cantidad,
+        });
+        console.log(carrito);
+        console.log(carrito.length);
+        carritoCounter();
+        saveLocal();
+      }
+      
+    });
+  });
+}
+
+getProduct();
+
+
+
+//set item
+const saveLocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+//get item
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+
